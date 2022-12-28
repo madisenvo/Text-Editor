5e8b9d2b3ead8f6e230d5c6d07b1439ff600ec79
Madisen Vogel <madisen@Admins-Mac-mini.btm.private>
2022-12-28 12:53:40 -0700

    install js logic

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -1,11 +1,28 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+// Event handler to the `beforeinstallprompt` event
+window.addEventListener('beforeinstallprompt', (event) => {
+    window.deferredPrompt = event;
+    butInstall.classList.toggle('hidden', false);
+});
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+// Event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt;
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+    if (!promptEvent) {
+        return;
+  }
+  // Shows prompt
+  promptEvent.prompt();
+  // Resets the deferred prompt variable
+  window.deferredPrompt = null;
+  butInstall.classList.toggle('hidden', true);
+});
+
+// Event handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+    // Clears prompt
+    window.deferredPrompt = null;
+});
