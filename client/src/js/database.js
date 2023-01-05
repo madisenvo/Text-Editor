8b9edccccd2f3aa68927b168bc1a52835dcc5ac5
madisenvo <109637640+madisenvo@users.noreply.github.com>
2023-01-05 13:51:46 -0700

    Update database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,30 +15,20 @@ const initdb = async () =>
 // Accepts content and adds it to the database
 export const putDb = async (content) => {
   console.error('putDb not implemented');
-  // creates a connection to the database
   const jateDB = await openDB('jate', 1);
-  // creates a transaction and specifies database and privileges
   const tx = jateDB.transaction('jate', 'readwrite');
-  // opens desired object store
   const store = tx.objectStore('jate');
-  // adds content to the database
-  const request = store.put({ jate: content });
-  // gets confirmation of the request
+  const request = store.add({ todo: content });
   const result = await request;
   console.log('Data successfully saved to the database.', result);
 };
 // Gets all the content from the database
 export const getDb = async () => {
   console.error('getDb not implemented');
-  // creates a connection to the database
   const jateDB = await openDB('jate', 1);
-  // creates a transaction and specifies database and privileges
   const tx = jateDB.transaction('jate', 'readonly');
-  // opens desired object store
   const store = tx.objectStore('jate');
-  // gets all the content from the database
   const request = store.get(1);
-  // gets confirmation of the request
   const result = await request;
   console.log('result.value', result);
   return result?.jate;
